51c80f715da14015bad069d83d4e3789b92f2f86
sneha <sneha.shinde350@gmail.com>
2021-02-24 02:39:27 -0500

    Make calc.py output as one dictionary

--- README.md
+++ README.md
@@ -4,7 +4,10 @@ Pipeline to measure cilia, cell nucleis, and which cilia are close to which cell
 # Components
 1) CellProfiler pipeline -- This collects the number of cell nuclei and cilia, as well as where they are in the image (their centres) & their sizes/shapes. It also saves the trace of nuclei/cilia seen in the CP pipeline.
 
-2) Python script centertocenter.py -- This finds out which cilia is closest to which cell given the above pipeline, assuming one cell:one cilia ratio and a threshold length away they can be (in pixels). 
+2) Python script centertocenter.py -- This finds out which cilia is closest to which nuclei given the above pipeline, assuming one cell:one cilia ratio and a threshold length away they can be (in pixels). 
 
-3) Python script label_cp_im.py -- This takes saved images of cells and cilia from the pipeline, and numbers them based on their centres in order to visualize the pipeline.
-4) Python script visualize.py -- This does the same thing as above, but superimposes the cilia numbers on the cell pictures and draws a line from the closest cilia to the closest cell to visualize the centertocenter script.  
+3) Python script label_cp_im.py -- This takes saved images of nuclei and cilia from the pipeline, and numbers them based on their centres in order to visualize the pipeline.
+
+4) Python script visualize.py -- This does the same thing as above, but superimposes the cilia numbers on the nuclei pictures and draws a line from the closest cilia to the closest cell to visualize the centertocenter script.  
+
+5) Python script calc.py -- This calculates mean and standard deviation of the columns 'ImageNumber', 'AreaShape_Area', 'AreaShape_MajorAxisLength', and 'AreaShape_MinorAxisLength' for both nuclei and cilia and outputs it as a dictionary.  
--- calc.py
+++ calc.py
@@ -12,36 +12,38 @@ def main():
     grouped_cell = cell_df.groupby(['ImageNumber'])
     cilia_df = pd.read_csv(cilia_csv_path, skipinitialspace=True, usecols=fields)
     grouped_cilia = cilia_df.groupby(['ImageNumber'])
+    output = dict()
     for num in range(1, num_im+1):
-        calc_dict_cell, calc_dict_cilia = make_lists(num, grouped_cell, grouped_cilia)
-        print(calc_dict_cell)
-        print(calc_dict_cilia)
+        calc_dict_cell, calc_dict_cilia = make_and_calc(num, grouped_cell, grouped_cilia)
+        output.update(calc_dict_cell)
+        output.update(calc_dict_cilia)
+
+    print(output)
+
+# makes list and calls calc mean func
+def make_and_calc(im_num, grouped_cell, grouped_cilia): 
+    im_df_cell = grouped_cell.get_group(im_num) 
+    im_df_cell.drop('ImageNumber', axis=1, inplace=True)
+    im_df_cilia = grouped_cilia.get_group(im_num) 
+    im_df_cilia.drop('ImageNumber', axis=1, inplace=True)
+
+    output_dict_cell = calc_mean(im_df_cell, im_num, '_cell')
+    output_dict_cilia = calc_mean(im_df_cell, im_num, '_cilia')
+    return output_dict_cell, output_dict_cilia
 
 # calculates mean and puts it into dict 
-def calc_mean(df, im_num):
+def calc_mean(df, im_num, thing):
     output_dict =dict()
     for field in fields:
         if not field == 'ImageNumber':
             mean = df[field].mean()
             stdev =df[field].std()
-            mean_name='im_' + str(im_num) + '_mean_' + field
-            stdev_name='im_' + str(im_num) + '_stdev_' + field
+            mean_name='im_' + str(im_num) + thing + '_mean_' + field
+            stdev_name='im_' + str(im_num) + thing + '_stdev_' + field
             add_to_dict = {mean_name: mean, stdev_name: stdev}
             output_dict.update(add_to_dict)
     return output_dict
 
-# makes lists
-def make_lists(im_num, grouped):
-    im_df = grouped.get_group(im_num) 
-    im_df.drop('ImageNumber', axis=1, inplace=True)
-    calc_dict = calc_mean(im_df, im_num)
-    return calc_dict
-
-def make_and_calccalc(im_num, grouped_cell, grouped_cilia): 
-    calc_dict_cell = make_lists(im_num, grouped_cell)
-    calc_dict_cilia = make_lists(im_num, grouped_cilia)
-
-    return calc_dict_cell, calc_dict_cilia
 
 if __name__ == "__main__":
     main()
