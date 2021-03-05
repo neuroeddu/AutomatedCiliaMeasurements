775ec9b73c217e316636955cc35b4dcf18c65b89
sneha <sneha.shinde350@gmail.com>
2021-03-05 18:00:46 -0500

    Calc.py output changed to csv

--- calc.py
+++ calc.py
@@ -4,6 +4,7 @@ import pandas as pd
 cell_csv_path='/Users/sneha/Desktop/mni/cilia-output/MyExpt_Cell.csv'
 cilia_csv_path='/Users/sneha/Desktop/mni/cilia-output/MyExpt_Cilia.csv'
 fields = ['ImageNumber', 'AreaShape_Area', 'AreaShape_MajorAxisLength', 'AreaShape_MinorAxisLength']
+csv_path ='/Users/sneha/Desktop/mni/tester.csv'
 ################################# TO CHANGE #################################
 
 def main(): 
@@ -12,38 +13,45 @@ def main():
     grouped_cell = cell_df.groupby(['ImageNumber'])
     cilia_df = pd.read_csv(cilia_csv_path, skipinitialspace=True, usecols=fields)
     grouped_cilia = cilia_df.groupby(['ImageNumber'])
-    output = dict()
+    output = []
     for num in range(1, num_im+1):
-        calc_dict_cell, calc_dict_cilia = make_and_calc(num, grouped_cell, grouped_cilia)
-        output.update(calc_dict_cell)
-        output.update(calc_dict_cilia)
+        make_and_calc(num, grouped_cell, grouped_cilia, output)
 
     print(output)
+    convert_dict_to_csv(output)
 
 # makes list and calls calc mean func
-def make_and_calc(im_num, grouped_cell, grouped_cilia): 
+def make_and_calc(im_num, grouped_cell, grouped_cilia, output): 
     im_df_cell = grouped_cell.get_group(im_num) 
     im_df_cell.drop('ImageNumber', axis=1, inplace=True)
     im_df_cilia = grouped_cilia.get_group(im_num) 
     im_df_cilia.drop('ImageNumber', axis=1, inplace=True)
 
-    output_dict_cell = calc_mean(im_df_cell, im_num, '_cell')
-    output_dict_cilia = calc_mean(im_df_cell, im_num, '_cilia')
-    return output_dict_cell, output_dict_cilia
+    output.append(calc_mean(im_df_cell, im_num, 'Cell'))
+    output.append(calc_mean(im_df_cell, im_num, 'Cilia'))
 
 # calculates mean and puts it into dict 
 def calc_mean(df, im_num, thing):
-    output_dict =dict()
-    for field in fields:
-        if not field == 'ImageNumber':
-            mean = df[field].mean()
-            stdev =df[field].std()
-            mean_name='im_' + str(im_num) + thing + '_mean_' + field
-            stdev_name='im_' + str(im_num) + thing + '_stdev_' + field
-            add_to_dict = {mean_name: mean, stdev_name: stdev}
-            output_dict.update(add_to_dict)
-    return output_dict
+    # so first i need to set the im and the thing
+    #                     0           1         2         3           4          5         6         7
+    #  # what do i want ['Im', 'cellorcilia', 'MeanF1', 'Stdev F1', 'MeanF2', 'stdevf2', 'meanf3', 'stdevf3']
+    cur_row = []
+    cur_row.append(str(im_num))
+    cur_row.append(thing)
+    for num, field in enumerate(fields): 
+        if field != 'ImageNumber':
+            mean = df[field].mean() # mean of our current field
+            stdev =df[field].std() # stdev of our current field
+            cur_row.append(mean)
+            cur_row.append(stdev)
+    
+    return cur_row
 
+
+def convert_dict_to_csv(output):
+    df = pd.DataFrame.from_dict(output)
+    df.index = df.index + 1
+    result = df.to_csv(path_or_buf=csv_path, header=["Image Num", "Type of image", "Mean AreaShape_Area", "Stdev AreaShape_Area", "Mean AreaShape_MajorAxisLength", "Stdev AreaShape_MajorAxisLength","Mean AreaShape_MinorAxisLength", "Stdev AreaShape_MinorAxisLength",], index=False)
 
 if __name__ == "__main__":
     main()
